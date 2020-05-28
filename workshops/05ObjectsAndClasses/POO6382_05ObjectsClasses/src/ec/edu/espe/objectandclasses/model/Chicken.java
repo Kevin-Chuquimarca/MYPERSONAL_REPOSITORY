32e6ed39ba62eb7d41e86411880385e4f1caeea6
Kevin-Chuquimarca <51093003+Kevin-Chuquimarca@users.noreply.github.com>
2020-05-28 13:39:34 -0500

    06 search nouns

--- workshops/05ObjectsAndClasses/POO6382_05ObjectsClasses/src/ec/edu/espe/objectandclasses/model/Chicken.java
+++ workshops/05ObjectsAndClasses/POO6382_05ObjectsClasses/src/ec/edu/espe/objectandclasses/model/Chicken.java
@@ -9,21 +9,50 @@ package ec.edu.espe.objectandclasses.model;
  *
  * @author santi
  */
-public class Chicken { 
+public class Chicken {
+
     private String name;
     private String color;
     private int age;
-    private boolean nolting;
-    
-    public void doStuff(){
+    private boolean isMolting;
+
+    public void doStuff() {
+        cluck();
+        wander();
+        eat();
+        drink();
+        poop();
         
+        layAndEgg();
     }
-    
-    private void cluck(){
+
+    private void cluck() {
         System.out.println("The chicken " + name + " is clucking");
     }
-    
-    private void wander(){
+
+    private void wander() {
         System.out.println("The chicken " + name + " is wandering");
+    }
+
+    private void eat() {
+        System.out.println("The chicken " + name + " is eating");
+    }
+
+    private void drink() {
+        System.out.println("The chicken " + name + " is drinking");
+    }
+
+    private Poop poop() {
+        Poop poopObject = new Poop(); //instance of Poop
+        System.out.println("The chicken " + name + " is pooping");
+        return poopObject;
+    }
+
+    private Egg layAndEgg() {
+        Egg egg = new Egg();
+        if (!isMolting) {
+            System.out.println("The chicken " + name + " is laying and egg");
+        }
+        return null;
     }
 }
