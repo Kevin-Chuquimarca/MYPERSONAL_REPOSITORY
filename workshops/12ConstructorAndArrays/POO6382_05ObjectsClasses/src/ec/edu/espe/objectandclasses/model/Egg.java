a54ff4df667187d9ed57cc8b6125cd6308bb2a47
Kevin-Chuquimarca <51093003+Kevin-Chuquimarca@users.noreply.github.com>
2020-06-17 12:16:13 -0500

    12 constructors and arrays

---
+++ workshops/12ConstructorAndArrays/POO6382_05ObjectsClasses/src/ec/edu/espe/objectandclasses/model/Egg.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.espe.objectandclasses.model;
+
+/**
+ *
+ * @author santi
+ */
+class Egg {
+    private int id;
+
+    public Egg(int id) {
+        this.id = id;
+    }
+ 
+}
